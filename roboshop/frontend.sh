8197a07b6ac16c0ed901c1361b480edf3437cda8
Kiran..! <prudhviraj3525@gmail.com>
2024-11-25 18:06:00 +0530

    b

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,11 +1,15 @@
 dnf module disable nginx -y
+
 dnf module enable nginx:1.24 -y
+
 dnf install nginx -y
+
 cp nginx.conf  /etc/nginx/nginx.conf
 
 
 rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zipcd /usr/share/nginx/html 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 unzip /tmp/frontend.zip
 cd /usr/share/nginx/html
 
